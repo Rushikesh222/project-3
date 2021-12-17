4f11b299fe78c3ae136cb4446a4cbbc3309485dd
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-12-17 20:34:52 +0530

    scroll magic

---
+++ app.js
@@ -0,0 +1,22 @@
+// init controller
+var controller = new ScrollMagic.Controller({
+  globalSceneOptions: { duration: 100 },
+});
+
+// build scenes
+new ScrollMagic.Scene({ triggerElement: "#sec1" })
+  .setClassToggle("#hh", "active") // add class toggle
+  .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+new ScrollMagic.Scene({ triggerElement: "#sec2" })
+  .setClassToggle("#hh", "active") // add class toggle
+  .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+new ScrollMagic.Scene({ triggerElement: "#sec3" })
+  .setClassToggle("#hh", "active") // add class toggle
+  .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+new ScrollMagic.Scene({ triggerElement: "#sec4" })
+  .setClassToggle("#hh", "active") // add class toggle
+  .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
