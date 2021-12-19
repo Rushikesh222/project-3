724f5001141b89bc51e91c2c1ce2afe8eba47e22
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-12-19 12:36:44 +0530

    background is remain

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ new ScrollMagic.Scene({ triggerElement: "#sec1" })
   .setClassToggle("#hh", "active") // add class toggle
   .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
-new ScrollMagic.Scene({ triggerElement: "#sec2" })
-  .setClassToggle("#hh", "active") // add class toggle
-  .addIndicators() // add indicators (requires plugin)
-  .addTo(controller);
-new ScrollMagic.Scene({ triggerElement: "#sec3" })
-  .setClassToggle("#hh", "active") // add class toggle
-  .addIndicators() // add indicators (requires plugin)
-  .addTo(controller);
-new ScrollMagic.Scene({ triggerElement: "#sec4" })
-  .setClassToggle("#hh", "active") // add class toggle
-  .addIndicators() // add indicators (requires plugin)
-  .addTo(controller);
+// new ScrollMagic.Scene({ triggerElement: "#sec2" })
+//   .setClassToggle("#hhb", "headingUp") // add class toggle
+//   // .addIndicators() // add indicators (requires plugin)
+//   .addTo(controller);
+// new ScrollMagic.Scene({ triggerElement: "#sec3" })
+//   .setClassToggle("#hh", "active") // add class toggle
+//   .addIndicators() // add indicators (requires plugin)
+//   .addTo(controller);
+// new ScrollMagic.Scene({ triggerElement: "#sec4" })
+//   .setClassToggle("#hh", "active") // add class toggle
+//   .addIndicators() // add indicators (requires plugin)
+//   .addTo(controller);
