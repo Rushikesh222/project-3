4dafced04b7807ac238ad71af2a05bb439c90c48
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-12-20 21:00:13 +0530

    final touch is remaining

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var controller = new ScrollMagic.Controller({
 // build scenes
 new ScrollMagic.Scene({ triggerElement: "#sec1" })
   .setClassToggle("#hh", "active") // add class toggle
-  .addIndicators() // add indicators (requires plugin)
+  // .addIndicators() // add indicators (requires plugin)
   .addTo(controller);
 // new ScrollMagic.Scene({ triggerElement: "#sec2" })
 //   .setClassToggle("#hhb", "headingUp") // add class toggle
